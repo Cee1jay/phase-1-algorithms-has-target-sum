5a93092e9357eccaff1a53eb2c5ad6680a41ad4f
Cee1jay <91193206+Cee1jay@users.noreply.github.com>
2022-06-14 18:41:46 +0300

    target sum

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-function hasTargetSum(array, target) {
+function isPalindrome(word) {
   // Write your algorithm here
 }
-
-/* 
-  Write the Big O time complexity of your function here
-*/
+function isPalindrome(word) {
+  if (word.length <= 1) {
+    return true;
+  } else {
+    if (word[0] === word[word.length - 1]) {
+      return isPalindrome(word.slice(1, -1));
+    } else {
+      return false;
+    }
+  }
+}
 
 /* 
   Add your pseudocode here
@@ -18,17 +25,12 @@ function hasTargetSum(array, target) {
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
-  console.log("");
-
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+  console.log("=>", isPalindrome("racecar"));
 
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+  console.log("=>", isPalindrome("robot"));
 }
 
-module.exports = hasTargetSum;
+module.exports = isPalindrome;
